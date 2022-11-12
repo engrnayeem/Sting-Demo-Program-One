c49b4ac190d9c72c683b8907ae03f8e685a7dbe5
unknown <engr.mohammadjannatulnayeem@gmail.com>
2022-11-13 01:30:29 +0600

    StringOneDemoProgram

---
+++ src/StringOneDemoProgram/StringOneDemoProgram.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package StringOneDemoProgram;
+
+/**
+ *
+ * @author nayeem
+ */
+public class StringOneDemoProgram {
+    public static void main(String[]args)
+    {
+    
+        //aivabe string declare korle normaly memory te jaiga kom lage.karon aitate sa khetre (string)ek bar e memory te jaiga nibe  joto bar e aivabe string declare kora hok na keno. string ta airokom  jodi aro declare kori tahole tara vinno vinno jaiga dhokhol kore na.
+        String s="Mohammad Nayeem";
+        String sss="Mohammad Nayeem";
+        
+        //System.out.println(s);
+       
+        
+        //r aivabe  string declare korle memory te jaiga basi lage.karon aivabe jotobar e string declare korbo totobar e  sa(String)memory te jaiga nibe.karon aivabe new diya  string declare korle sa(string) potibar e memory te notun notun jaiga nibe.
+        String ss =new String("Mohammad Nayeem");
+        
+        //System.out.println(ss);
+       
+        /**if(s.equals(ss) )
+            {
+            System.out.println("Equals");
+            
+            }
+        else
+            {
+            
+            System.out.println("Not Equals");
+            }
+        **/
+        
+        /**
+        if(s==(ss) )
+         //s==ss khokhono equal hobe na karon duitar memory location vinno. 
+           {
+            System.out.println("Equals");
+            
+            }
+        else
+            {
+            
+            System.out.println("Not Equals");
+            }
+        **/
+        
+        
+        /**
+        if(s==(sss) )
+            {
+            System.out.println("Equals");
+            
+            }
+        else
+            {
+            
+            System.out.println("Not Equals");
+            }
+        **/
+        
+        char[] MyArray={'a','b','c','d','e'};
+        System.out.println(MyArray);
+        
+        
+        
+        
+        
+    
+    
+    
+    }
+    
+}
